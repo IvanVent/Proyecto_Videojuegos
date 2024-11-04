10fa1f36d602f71b116aac2478ccf2d4f76e04b4
Ivan Ventura Segui <ivan.ventura@alumnos.upm.es>
2024-11-04 13:50:58 +0100

    main - Segundo enemigo implementado con animacion
    Creacion del segundo tipo de enemigo con su animacion de daño ya puesta

---
+++ Proyecto_Videojuegos/Assets/Scripts/BigEnemy.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigEnemy : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private float speed;
+    [SerializeField] private float hp=6;
+
+
+    private Animator animator;
+    private bool isFacingRight = true;
+    private bool receivingDamage = false;
+
+    void Start(){
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        bool isPlayerRight = transform.position.x < player.position.x;
+        Flip(isPlayerRight);
+        animator.SetBool("Damaged", receivingDamage);
+    }
+
+    private void Flip(bool isPlayerRight)
+    {
+        if (isPlayerRight && !isFacingRight || !isPlayerRight && isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+
+    //se hace que la hitbox del enemigo sea un trigger para que atraviese al jugador mientras que al mismo tiempo,  se destruya las flechas
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Arrow"))
+        {
+            hp--;
+            receivingDamage = true;
+            Debug.Log(receivingDamage);
+            if (hp <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void DeactivateDamage()
+    {
+        receivingDamage = false;
+    }
+
+}
