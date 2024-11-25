ccbae9cc48d001ce4aa37c43336fa271bfa23c7e
Daniel Paz <danielpazp23@gmail.com>
2024-11-25 21:19:47 +0100

    Mejoras en Scripts Procedural

--- Proyecto_Videojuegos/Assets/Scripts/Procedural/TileMap.cs
+++ Proyecto_Videojuegos/Assets/Scripts/Procedural/TileMap.cs
@@ -12,22 +12,21 @@ using Random = UnityEngine.Random;
 public class TileMap : MonoBehaviour
 {
     public GameObject roomPrefab;
+    /*
     public GameObject upDoorPrefab;
     public GameObject downDoorPrefab;
     public GameObject leftDoorPrefab;
     public GameObject rightDoorPrefab;
-    
-    public SmallEnemy enemy1Prefab;
-    public BigEnemy enemy2Prefab;
+    */
+    //public SmallEnemy enemy1Prefab;
+    //public BigEnemy enemy2Prefab;
 
     private GameObject room;
 
-    public Transform roomsFather;
-    public Transform enemiesFather;
-    public Transform doorsFather;
+    public Transform roomsFather; // Objeto vacío donde estarán las instancias de las habitaciones
     
-    private List<GameObject> doorWallColliders = new List<GameObject>();
-    private List<GameObject> normalColliders = new List<GameObject>();
+    //private List<GameObject> doorWallColliders = new List<GameObject>();
+    //private List<GameObject> normalColliders = new List<GameObject>();
     float roomWidth = 9f;
     float roomHeight = 10f;
     
@@ -37,126 +36,35 @@ public class TileMap : MonoBehaviour
     {
         ClearTileMap();
     }
+    
     // Este método se llama cada vez que se recorre una celda de la matriz que define el mapa.
     public void AddRoom(float x, float y, int id = 0)
     {
-        
         var roomPos = new Vector3(x * roomWidth * 2, y * roomHeight, 1f);
         
-        
-        // Si la habitación actual no tiene puertas (id == 0) no se instancia la habitación, es decir, 
-        // hay un hueco vacío en el mapa.
+        /* Si la habitación actual no tiene puertas (id == 0) no se instancia la habitación, es decir, 
+        hay un hueco vacío en el mapa. */
         if (id != 0)
         {
-            
-            
             room = Instantiate(roomPrefab, roomPos, Quaternion.identity);
             room.transform.SetParent(roomsFather);
             
             roomController = room.GetComponent<RoomController>();
-            roomController.SetEnemiesFather(enemiesFather);
-            roomController.SetSmallEnemyPrefab(enemy1Prefab);
-            roomController.SetBigEnemyPrefab(enemy2Prefab);
-            
-            room.GetComponent<RoomController>().AddEnemies(new Vector3(roomPos.x - 4, roomPos.y - 1, 0));
-            
-            /*
-            var doorWallCollider_obj = room.transform.Find("DoorColliders");
-            var normalCollider_obj = room.transform.Find("Colliders");
-            
-            doorWallColliders = new List<GameObject>();
-            normalColliders = new List<GameObject>();
-            // Se guardan todos los colliders de las paredes sin puerta y se desactivan. Se activan en
-            // AddDoors() solo los que hagan falta.
-            foreach (Transform child in doorWallCollider_obj.transform)
-            {
-                doorWallColliders.Add(child.gameObject);
-                child.gameObject.SetActive(false);
-            }
-
-            // Se guardan todos los colliders de las paredes con puerta.
-            foreach (Transform child in normalCollider_obj.transform)
-            {
-                normalColliders.Add(child.gameObject);
-            }*/
-            AddDoors(id, roomPos);
+            roomController.SetRoomID(id);
+            roomController.AddEnemies(new Vector3(roomPos.x - 4, roomPos.y - 1, 0));
+            roomController.AddDoors(roomPos);
         }
-
     }
-
-    // Método que añade las puertas a una habitación, ajustando los colliders de la habitación para adaptarse a ellas.
-    private void AddDoors(int id, Vector3 roomPos)
-    {
-        var doorPos = new Vector3();
-        GameObject door;
-        
-        // Este bucle recorre los bits del id. Dependiendo del bit que esté a 1, se añade una puerta donde corresponde y 
-        // se ajustan los colliders.
-        for (int i = 0; i < 4; i++)
-        {
-            int mask = 1 << i;
-            if ((id & mask) != 0)
-            {
-                if (i == 0) // puerta arriba
-                {
-                    doorPos.x = roomPos.x- 2;
-                    doorPos.y = roomPos.y + 4;
-                    
-                    door = Instantiate(upDoorPrefab, doorPos, Quaternion.identity);
-                    door.transform.SetParent(doorsFather);
-                    
-                    doorWallColliders[0].gameObject.SetActive(true);
-                    normalColliders[0].gameObject.SetActive(false);
-                }
-                else if (i == 1) // puerta derecha
-                {
-                    doorPos.x = roomPos.x + 7;
-                    doorPos.y = roomPos.y;
-                    door = Instantiate(rightDoorPrefab, doorPos, Quaternion.identity);
-                    door.transform.SetParent(doorsFather);
-                    doorWallColliders[1].gameObject.SetActive(true);
-                    normalColliders[1].gameObject.SetActive(false);
-                }
-                else if (i == 2) // puerta abajo
-                {
-                    doorPos.x = roomPos.x-2;
-                    doorPos.y = roomPos.y - 4;
-                    door = Instantiate(downDoorPrefab, doorPos, Quaternion.identity);
-                    door.transform.SetParent(doorsFather);
-                    doorWallColliders[2].gameObject.SetActive(true);
-                    normalColliders[2].gameObject.SetActive(false);
-                }
-                else if (i == 3) // puerta izquierda
-                {
-                    doorPos.x = roomPos.x - 10;
-                    doorPos.y = roomPos.y;
-                    door = Instantiate(leftDoorPrefab, doorPos, Quaternion.identity);
-                    door.transform.SetParent(doorsFather);
-                    doorWallColliders[3].gameObject.SetActive(true);
-                    normalColliders[3].gameObject.SetActive(false);
-                }
-            }
-        }//for
-    }//AddDoors
-
-    // elimina todos las habitaciones, enemigos, puertas y colliders
+    
+    // elimina todas las habitaciones, enemigos, puertas y colliders
     public void ClearTileMap()
     {
-
+        // clear de las habitaciones
         foreach (Transform roomObj in roomsFather.transform)
         {
             Destroy(room.gameObject);
         }
-        foreach (Transform door in doorsFather.transform)
-        {
-            Destroy(door.gameObject);
-        }
-        foreach (Transform enemy in enemiesFather.transform)
-        {
-            Destroy(enemy.gameObject);
-        }
-        doorWallColliders.Clear();
-        normalColliders.Clear();
+        //roomController.ClearRoom();
     }
 
 }
--- Proyecto_Videojuegos/Assets/Scripts/RoomController.cs
+++ Proyecto_Videojuegos/Assets/Scripts/RoomController.cs
@@ -6,50 +6,50 @@ using Random = UnityEngine.Random;
 
 public class RoomController : MonoBehaviour
 {
-    
-    private Transform enemiesFather;
-    private SmallEnemy enemy1Prefab;
-    private BigEnemy enemy2Prefab;
+    public SmallEnemy enemy1Prefab;
+    public BigEnemy enemy2Prefab;
     
     private List<SmallEnemy> smallEnemiesList = new List<SmallEnemy>();
     private List<BigEnemy> bigEnemiesList = new List<BigEnemy>();
     
     private List<GameObject> doorWallColliders = new List<GameObject>();
     private List<GameObject> normalColliders = new List<GameObject>();
+    
+    public GameObject upDoorPrefab;
+    public GameObject downDoorPrefab;
+    public GameObject leftDoorPrefab;
+    public GameObject rightDoorPrefab;
+    
+    private Transform enemiesFather; // Objeto vacío donde estarán las instancias de las habitaciones
+    private Transform doorsFather; // " " puertas. No será hijo del room para que su trigger no interfiera con el del room
 
     private int enemyCount = 1;
+    private int room_id;
     
-    void Start()
+    private void Awake()
     {
         LoadColliders();
-
-        
+        enemiesFather = transform.Find("Enemies");
+        doorsFather = GameObject.Find("Doors").transform;
     }
 
-    public void SetEnemiesFather(Transform enemiesFather)
+    public void SetRoomID(int id)
     {
-        this.enemiesFather = enemiesFather;
+        this.room_id = id;
     }
 
-    public void SetSmallEnemyPrefab(SmallEnemy prefab1)
-    {
-        this.enemy1Prefab = prefab1;
-    }
-
-    public void SetBigEnemyPrefab(BigEnemy prefab2)
-    {
-        this.enemy2Prefab = prefab2;
-    }
-
+    // Guarda los colliders de una habitación en listas para luego acceder a ellos facilmente
     public void LoadColliders()
     {
+
         var doorWallCollider_obj = transform.Find("DoorColliders");
         var normalCollider_obj = transform.Find("Colliders");
-        
+
         // Se guardan todos los colliders de las paredes sin puerta y se desactivan. Se activan en
         // AddDoors() solo los que hagan falta.
         foreach (Transform child in doorWallCollider_obj.transform)
         {
+            
             doorWallColliders.Add(child.gameObject);
             child.gameObject.SetActive(false);
         }
@@ -60,6 +60,61 @@ public class RoomController : MonoBehaviour
             normalColliders.Add(child.gameObject);
         }
     }
+    
+    // Añade las puertas a una habitación, ajustando los colliders de la habitación para adaptarse a ellas.
+    public void AddDoors(Vector3 roomPos)
+    {
+        var doorPos = new Vector3();
+        GameObject door;
+        
+        // Este bucle recorre los bits del id. Dependiendo del bit que esté a 1, se añade una puerta donde corresponde y 
+        // se ajustan los colliders.
+        for (int i = 0; i < 4; i++)
+        {
+            int mask = 1 << i;
+            if ((room_id & mask) != 0)
+            {
+                if (i == 0) // puerta arriba
+                {
+                    doorPos.x = roomPos.x- 2;
+                    doorPos.y = roomPos.y + 4;
+                    
+                    door = Instantiate(upDoorPrefab, doorPos, Quaternion.identity);
+                    door.transform.SetParent(doorsFather);
+                    
+                    doorWallColliders[0].gameObject.SetActive(true);
+                    normalColliders[0].gameObject.SetActive(false);
+                }
+                else if (i == 1) // puerta derecha
+                {
+                    doorPos.x = roomPos.x + 7;
+                    doorPos.y = roomPos.y;
+                    door = Instantiate(rightDoorPrefab, doorPos, Quaternion.identity);
+                    door.transform.SetParent(doorsFather);
+                    doorWallColliders[1].gameObject.SetActive(true);
+                    normalColliders[1].gameObject.SetActive(false);
+                }
+                else if (i == 2) // puerta abajo
+                {
+                    doorPos.x = roomPos.x-2;
+                    doorPos.y = roomPos.y - 4;
+                    door = Instantiate(downDoorPrefab, doorPos, Quaternion.identity);
+                    door.transform.SetParent(doorsFather);
+                    doorWallColliders[2].gameObject.SetActive(true);
+                    normalColliders[2].gameObject.SetActive(false);
+                }
+                else if (i == 3) // puerta izquierda
+                {
+                    doorPos.x = roomPos.x - 10;
+                    doorPos.y = roomPos.y;
+                    door = Instantiate(leftDoorPrefab, doorPos, Quaternion.identity);
+                    door.transform.SetParent(doorsFather);
+                    doorWallColliders[3].gameObject.SetActive(true);
+                    normalColliders[3].gameObject.SetActive(false);
+                }
+            }
+        }//for
+    }//AddDoors
     
     //crea una instancia de uno de los dos enemigos
     public void AddEnemies(Vector3 pos)
@@ -85,9 +140,28 @@ public class RoomController : MonoBehaviour
         
     }
     
+    // Elimina todas las intancias de enemigos, colliders y puertas de la habitacion
+    public void ClearRoom()
+    {
+        // clear de las puertas
+        foreach (Transform door in doorsFather.transform)
+        {
+            Destroy(door.gameObject);
+        }
+        // clear de los enemigos
+        foreach (Transform enemy in enemiesFather.transform)
+        {
+            Destroy(enemy.gameObject);
+        }
+        // clear de los colliders
+        doorWallColliders.Clear();
+        normalColliders.Clear();
+    }
+    
     // Activa los enemigos para perseguir al jugador cuando entre en la habitacion
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.gameObject.CompareTag("Player"))
         {
             foreach (SmallEnemy e1 in smallEnemiesList)
@@ -105,6 +179,7 @@ public class RoomController : MonoBehaviour
     // Desactiva los enemigos para perseguir al jugador cuando entre en la habitacion
     private void OnTriggerExit2D(Collider2D collision)
     {
+        
         if (collision.gameObject.CompareTag("Player"))
         {
             foreach (SmallEnemy e1 in smallEnemiesList)
