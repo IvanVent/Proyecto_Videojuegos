cc0859d2e0355a0762efaad5e0df5810ae848af1
ralaminos <r.alaminos@alumnos.upm.es>
2024-11-28 19:10:23 +0100

    Pickup con la E

--- Proyecto_Videojuegos/Assets/Scripts/PowerUps/PowerUpPickup.cs
+++ Proyecto_Videojuegos/Assets/Scripts/PowerUps/PowerUpPickup.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class PowerUpPickup : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool automatic=false;
     public void OnTriggerEnter2D(Collider2D trigger){
-        if(trigger.CompareTag("Player")){
-            PowerUp powerUp = GetComponent<PowerUp>();
-            if(powerUp!=null){
-                powerUp.OnPick(trigger.gameObject);
+        if(Input.GetKeyDown(KeyCode.E)||automatic){
+            if(trigger.CompareTag("Player")){
+                PowerUp powerUp=GetComponent<PowerUp>();
+                if(powerUp!=null){
+                    powerUp.OnPick(trigger.gameObject);
+                }
+            }
+        }
+    }
+    public void OnTriggerStay2D(Collider2D trigger){
+        if(Input.GetKeyDown(KeyCode.E)||automatic){
+            if(trigger.CompareTag("Player")){
+                PowerUp powerUp=GetComponent<PowerUp>();
+                if(powerUp!=null){
+                    powerUp.OnPick(trigger.gameObject);
+                }
             }
         }
     }
