88543b1930d4b3703ffb3f62723760f15f4d5022
Ivan Ventura Segui <ivan.ventura@alumnos.upm.es>
2025-01-03 21:18:28 +0100

    UI - definitivo

--- Proyecto_Videojuegos/Assets/Scripts/MenuInicial.cs
+++ Proyecto_Videojuegos/Assets/Scripts/MenuInicial.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuInicial : MonoBehaviour
 {
     
     //comienza una partida comentario editado
     public AudioSource src;
+    public GameObject optionsUI;
+    public VolumeControl volumeControl;
+    public Player player;
+    public Text optionText;
+
     private bool listo=true;
+    private bool options=false;
+
+    
     public void Update(){
         if(listo){
             StartCoroutine(musicamenu());
@@ -24,6 +33,7 @@ public class MenuInicial : MonoBehaviour
     public void Jugar(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //carga la siguiente escena configurada en el build settings
     }
+
 
     //sale del juego
     public void Salir(){
